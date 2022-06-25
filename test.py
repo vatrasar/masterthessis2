28c7e7353097350f792d81f12c0347b4554f18d1
vatrasar <vatrasar@gmail.com>
2022-06-25 18:27:47 +0200

    add gnuplot files

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("%.2f"%(22222.2))
+import random
+test={}
+for i in range(0,21):
+    test[i]=0
+for i in range(0,338):
+  test[int((random.random()*1040)/50.0)]=test[int((random.random()*1040)/50.0)]+1
+for i in test:
+    print(test[i])
